cf3aef56f0d6d0ece04afd11985fcbf27a82e85b
Sangmin Seo <sseo@anl.gov>
2014-06-10 09:02:35 -0500

    Template code modification.

--- maint/template.c
+++ maint/template.c
@@ -5,15 +5,9 @@
 
 #include "abti.h"
 
-int ABTI_template(ABTI_stream *p_stream, ABTI_thread *p_thread, void *p_arg)
+int ABTI_template(ABTI_thread *p_thread, void *p_arg)
 {
     int abt_errno = ABT_SUCCESS;
-
-    if (p_stream == NULL) {
-        HANDLE_ERROR("NULL STREAM");
-        abt_errno = ABT_ERR_INV_STREAM;
-        goto fn_fail;
-    }
 
     if (p_thread == NULL) {
         HANDLE_ERROR("NULL THREAD");
