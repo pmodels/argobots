ce1777aaa1414927b0fe1f1dee7e3ff3068f476e
Esteban Meneses <emenese2@illinois.edu>
2014-06-11 20:23:56 -0500

    Adding Fibonacci example using futures.

---
+++ test/fibonacci_future.c
@@ -0,0 +1,88 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ * See COPYRIGHT in top-level directory.
+ */
+
+/**
+ * This example shows the use of futures to compute Fibonacci numbers.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <abt.h>
+
+#define N 2
+
+/* structure to pass arguments to threads */
+typedef struct {
+	int n;
+	ABT_future future;
+} thread_args;
+
+/* Function to compute Fibonacci numbers */
+void fibonacci(void *arguments){
+	int n, result, *n1, *n2;
+	thread_args a1, a2;
+	ABT_thread t1, t2;
+	ABT_stream stream;
+	ABT_future future, f1, f2;
+
+	thread_args *args = (thread_args *) arguments;
+	n = args->n;
+	future = args->future;
+
+//printf("Computing Fibonacci of %d\n",n);
+	/* checking for base cases */
+	if(n <= 2)
+		result = 1;
+	else {
+		ABT_stream_self(&stream);
+		ABT_future_create(sizeof(int), stream, &f1);
+		a1.n = n-1;
+		a1.future = f1;
+    	ABT_thread_create(stream, fibonacci, &a1, 16384, &t1);
+		ABT_future_create(sizeof(int), stream, &f2);
+		a2.n = n-2;
+		a2.future = f2;
+    	ABT_thread_create(stream, fibonacci, &a2, 16384, &t2);
+		ABT_future_wait(f1, (void **) &n1);
+		ABT_future_wait(f2, (void **) &n2);
+		result = *n1 + *n2;
+	}
+
+	/* checking whether to signal the future */
+	if(future != ABT_FUTURE_NULL){
+		ABT_future_set(future, &result, sizeof(int));
+	} else {
+		printf("The %d-th number in the Fibonacci sequence is: %d\n",n,result);
+	}
+}
+
+/* Main function */
+int main(int argc, char *argv[])
+{
+	int n;
+	ABT_stream stream;
+	ABT_thread thread;
+	thread_args args;
+
+	/* init and thread creation */
+    ABT_init(argc, argv);
+	if(argc > 1)
+		n = atoi(argv[1]);
+	else
+		n = N;
+	args.n = n;
+	args.future = ABT_FUTURE_NULL;
+	ABT_stream_self(&stream);
+    ABT_thread_create(stream, fibonacci, &args, 16384, &thread);
+
+	/* switch to other user-level threads */
+	ABT_thread_yield();
+	
+	/* join other threads */
+	ABT_thread_join(thread);
+
+    ABT_finalize();
+    return 0;
+}
