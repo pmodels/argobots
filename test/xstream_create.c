025e343a5d4b8d4e2547cb976dad9127b8b61389
Sangmin Seo <sseo@anl.gov>
2014-06-27 13:06:11 -0500

    Adding an example for ES creation.
    This is an example for testing basic ES functions.

---
+++ test/xstream_create.c
@@ -0,0 +1,63 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ * See COPYRIGHT in top-level directory.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <abt.h>
+
+#define DEFAULT_NUM_XSTREAMS    4
+
+#define HANDLE_ERROR(ret,msg)                           \
+    if (ret != ABT_SUCCESS) {                           \
+        fprintf(stderr, "ERROR[%d]: %s\n", ret, msg);   \
+        exit(EXIT_FAILURE);                             \
+    }
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int ret;
+    int num_xstreams = DEFAULT_NUM_XSTREAMS;
+    if (argc > 1) num_xstreams = atoi(argv[1]);
+    assert(num_xstreams >= 0);
+
+    ABT_xstream *xstreams;
+    xstreams = (ABT_xstream *)malloc(sizeof(ABT_xstream) * num_xstreams);
+
+    /* Initialize */
+    ret = ABT_init(argc, argv);
+    HANDLE_ERROR(ret, "ABT_init");
+
+    /* Create Execution Streams */
+    ret = ABT_xstream_self(&xstreams[0]);
+    HANDLE_ERROR(ret, "ABT_xstream_self");
+    for (i = 1; i < num_xstreams; i++) {
+        ret = ABT_xstream_create(ABT_SCHED_NULL, &xstreams[i]);
+        HANDLE_ERROR(ret, "ABT_xstream_create");
+    }
+
+    /* Join Execution Streams */
+    for (i = 1; i < num_xstreams; i++) {
+        ret = ABT_xstream_join(xstreams[i]);
+        HANDLE_ERROR(ret, "ABT_xstream_join");
+    }
+
+    /* Free Execution Streams */
+    for (i = 1; i < num_xstreams; i++) {
+        ret = ABT_xstream_free(&xstreams[i]);
+        HANDLE_ERROR(ret, "ABT_xstream_free");
+    }
+
+    /* Finalize */
+    ret = ABT_finalize();
+    HANDLE_ERROR(ret, "ABT_finalize");
+
+    free(xstreams);
+
+    printf("No Error\n");
+
+    return EXIT_SUCCESS;
+}
