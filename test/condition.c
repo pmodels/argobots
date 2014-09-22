1297cf7c7387387d751fda34a2c59149fd5308f2
Sangmin Seo <sseo@anl.gov>
2014-09-22 16:41:32 -0500

    Bug fix in condition test code.
    First, the result validation was wrong. And, there was a race
    situation in the code because the single condition variable was
    used in two places. This patch fixed these two problems.
    
    Signed-off-by: Cyril Bordage <cbordage@illinois.edu>

--- test/condition.c
+++ test/condition.c
@@ -26,9 +26,11 @@ int num_threads = DEFAULT_NUM_THREADS;
 #define COUNT_LIMIT     15
 int g_counter = 0;
 int g_num_incthreads = 0;
+int g_waiting = 0;
 
 ABT_mutex mutex = ABT_MUTEX_NULL;
-ABT_cond  cond  = ABT_COND_NULL;
+ABT_cond cond = ABT_COND_NULL;
+ABT_cond broadcast = ABT_COND_NULL;
 
 typedef struct thread_arg {
     int sid;    /* stream ID */
@@ -47,9 +49,10 @@ void inc_counter(void *arg)
         g_counter++;
 
         if (g_counter == COUNT_LIMIT) {
-            ABT_cond_signal(cond);
             printf("[ES%d:TH%d] inc_counter(): threshold(%d) reached\n",
                    es_id, my_id, g_counter);
+            ABT_cond_signal(cond);
+            printf("[ES%d:TH%d] inc_counter(): sent signal\n", es_id, my_id);
         }
 
         ABT_mutex_unlock(mutex);
@@ -59,9 +62,8 @@ void inc_counter(void *arg)
 
     ABT_mutex_lock(mutex);
     g_num_incthreads++;
-    ABT_cond_wait(cond, mutex);
+    ABT_cond_wait(broadcast, mutex);
     ABT_mutex_unlock(mutex);
-    printf("[ES%d:TH%d] waken up\n", es_id, my_id);
 }
 
 void watch_counter(void *arg)
@@ -74,12 +76,11 @@ void watch_counter(void *arg)
 
     ABT_mutex_lock(mutex);
     while (g_counter < COUNT_LIMIT) {
+        printf("[ES%d:TH%d] watch_count(): waiting\n", es_id, my_id);
         ABT_cond_wait(cond, mutex);
-        printf("[ES%d:TH%d] watch_count(): condition signal received\n",
-               es_id, my_id);
+        printf("[ES%d:TH%d] watch_count(): received signal\n", es_id, my_id);
+        g_waiting = 1;
         g_counter += 100;
-        printf("[ES%d:TH%d] watch_count(): count now = %d\n",
-               es_id, my_id, g_counter);
     }
     ABT_mutex_unlock(mutex);
 
@@ -87,7 +88,7 @@ void watch_counter(void *arg)
         ABT_thread_yield();
     }
     ABT_mutex_lock(mutex);
-    ABT_cond_broadcast(cond);
+    ABT_cond_broadcast(broadcast);
     printf("[ES%d:TH%d] broadcast the condition\n", es_id, my_id);
     ABT_mutex_unlock(mutex);
 }
@@ -95,7 +96,7 @@ void watch_counter(void *arg)
 int main(int argc, char *argv[])
 {
     int i, j;
-    int ret;
+    int ret, expected;
     if (argc > 1) num_xstreams = atoi(argv[1]);
     assert(num_xstreams >= 0);
     if (argc > 2) num_threads = atoi(argv[2]);
@@ -132,19 +133,30 @@ int main(int argc, char *argv[])
     ret = ABT_mutex_create(&mutex);
     HANDLE_ERROR(ret, "ABT_mutex_create");
 
-    /* Create a condition variable */
+    /* Create condition variables */
     ret = ABT_cond_create(&cond);
+    HANDLE_ERROR(ret, "ABT_cond_create");
+    ret = ABT_cond_create(&broadcast);
     HANDLE_ERROR(ret, "ABT_cond_create");
 
     /* Create threads */
     args[0][0].sid = 0;
-    args[0][1].tid = 1;
+    args[0][0].tid = 1;
     ret = ABT_thread_create(xstreams[0], watch_counter, (void *)&args[0][0],
             ABT_THREAD_ATTR_NULL, NULL);
     HANDLE_ERROR(ret, "ABT_thread_create");
-    for (i = 0; i < num_xstreams; i++) {
+    i = 0;
+    for (j = 1; j < num_threads; j++) {
+        int tid = i * num_threads + j + 1;
+        args[i][j].sid = i;
+        args[i][j].tid = tid;
+        ret = ABT_thread_create(xstreams[i],
+                inc_counter, (void *)&args[i][j], ABT_THREAD_ATTR_NULL,
+                NULL);
+        HANDLE_ERROR(ret, "ABT_thread_create");
+    }
+    for (i = 1; i < num_xstreams; i++) {
         for (j = 0; j < num_threads; j++) {
-            if (i == 0 && j == 0) continue;
             int tid = i * num_threads + j + 1;
             args[i][j].sid = i;
             args[i][j].tid = tid;
@@ -168,8 +180,10 @@ int main(int argc, char *argv[])
     ret = ABT_mutex_free(&mutex);
     HANDLE_ERROR(ret, "ABT_mutex_free");
 
-    /* Free the condition variable */
+    /* Free the condition variables */
     ret = ABT_cond_free(&cond);
+    HANDLE_ERROR(ret, "ABT_cond_free");
+    ret = ABT_cond_free(&broadcast);
     HANDLE_ERROR(ret, "ABT_cond_free");
 
     /* Free Execution Streams */
@@ -182,14 +196,19 @@ int main(int argc, char *argv[])
     ret = ABT_finalize();
     HANDLE_ERROR(ret, "ABT_finalize");
 
-    printf("g_counter = %d (expected: %d)\n", g_counter,
-            100 + (num_xstreams * num_threads - 1) * TCOUNT);
-
     for (i = 0; i < num_xstreams; i++) {
         free(args[i]);
     }
     free(args);
     free(xstreams);
+
+    /* Validation */
+    expected = (num_xstreams * num_threads - 1) * TCOUNT + g_waiting * 100;
+    if (g_counter == expected) {
+        printf("No Error\n");
+    } else {
+        printf("g_counter = %d (expected: %d)\n", g_counter, expected);
+    }
 
     return EXIT_SUCCESS;
 }
