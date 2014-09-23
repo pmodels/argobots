e969a9ca04359c1f9686d2622b10810aa4aa1a23
Sangmin Seo <sseo@anl.gov>
2014-09-23 16:59:37 -0500

    Update directory structure and related files.
    Header files were moved to include directory, and existing test
    code files were relocated to test/basic directory. Most Makefile.am
    files and configure.ac have been updated.
    
    Signed-off-by: Cyril Bordage <cbordage@illinois.edu>

---
+++ src/include/Makefile.mk
@@ -0,0 +1,15 @@
+# -*- Mode: Makefile; -*-
+#
+# See COPYRIGHT in top-level directory.
+#
+
+include_HEADERS = include/abt.h
+
+noinst_HEADERS = \
+	include/config.h \
+	include/abtd.h \
+	include/abtd_atomic.h \
+	include/abti.h \
+	include/abti_error.h \
+	include/abtu.h
+
