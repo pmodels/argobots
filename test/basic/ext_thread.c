634bf719cde4247a32caaf4b8c4e58ce56d617f1
Sangmin Seo <sseo@anl.gov>
2015-03-27 15:37:01 -0500

    Add a test code ext_thread.c.
    The test code is to check if Argobots APIs work well when they are
    called by an external thread. The test code should not result in any
    problems.
    
    Signed-off-by: Huiwei Lu <huiweilu@mcs.anl.gov>

---
+++ test/basic/ext_thread.c
@@ -0,0 +1,226 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ * See COPYRIGHT in top-level directory.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <pthread.h>
+#include "abt.h"
+#include "abttest.h"
+
+/* This code tests that a pthread can call Argobots APIs without any problems.
+ * When a pthread calls Arogobots APIs, it might get an error code as a return
+ * value, but there should be no problems like segmentation fault. */
+
+#define BUF_SIZE 10
+
+ABT_mutex g_mutex = ABT_MUTEX_NULL;
+ABT_cond  g_cond  = ABT_COND_NULL;
+ABT_eventual g_eventual[2] = {ABT_EVENTUAL_NULL, ABT_EVENTUAL_NULL};
+ABT_future   g_future[2]   = {ABT_FUTURE_NULL, ABT_FUTURE_NULL};
+int g_counter = 0;
+int g_threads = 0;
+
+void task_func(void *arg)
+{
+    ABT_xstream xstream;
+    int rank, ret;
+    int my_id = (int)(size_t)arg;
+
+    ret = ABT_xstream_self(&xstream);
+    ABT_TEST_ERROR(ret, "ABT_xstream_self");
+    ret = ABT_xstream_self_rank(&rank);
+    ABT_TEST_ERROR(ret, "ABT_xstream_self_rank");
+
+    ABT_test_printf(1, "TASK %d: running on ES %d\n", my_id, rank);
+}
+
+void thread_func(void *arg)
+{
+    ABT_xstream xstream;
+    int rank, ret;
+    int my_id = (int)(size_t)arg;
+
+    ret = ABT_xstream_self(&xstream);
+    ABT_TEST_ERROR(ret, "ABT_xstream_self");
+    ret = ABT_xstream_self_rank(&rank);
+    ABT_TEST_ERROR(ret, "ABT_xstream_self_rank");
+
+    ABT_mutex_lock(g_mutex);
+    g_counter++;
+    ABT_mutex_unlock(g_mutex);
+
+    /* ULT are waiting, and pthread will broadcast the signal. */
+    ABT_mutex_lock(g_mutex);
+    g_threads++;
+    ABT_cond_wait(g_cond, g_mutex);
+    ABT_mutex_unlock(g_mutex);
+
+    /* ULT 1 and pthread are waiting, and ULT 0 broadcasts. */
+    if (my_id == 0) {
+        while (g_threads < 2) ABT_thread_yield();
+        ABT_mutex_lock(g_mutex);
+        ABT_cond_broadcast(g_cond);
+        ABT_mutex_unlock(g_mutex);
+    } else {
+        ABT_mutex_lock(g_mutex);
+        g_threads++;
+        ABT_cond_wait(g_cond, g_mutex);
+        ABT_mutex_unlock(g_mutex);
+    }
+
+    /* Test eventual */
+    void *evt_result;
+    char *buf_evt = (char *)malloc(BUF_SIZE);
+    ABT_eventual_wait(g_eventual[0], &evt_result);
+    if (my_id == 0) {
+        ABT_eventual_wait(g_eventual[1], &evt_result);
+    } else {
+        ABT_eventual_set(g_eventual[1], buf_evt, BUF_SIZE);
+    }
+    free(buf_evt);
+
+    /* Test future */
+    char *buf_fut = (char *)malloc(BUF_SIZE);
+    if (my_id == 0) {
+        ABT_future_set(g_future[0], (void *)buf_fut);
+    } else {
+        ABT_future_wait(g_future[0]);
+    }
+    ABT_future_wait(g_future[1]);
+    free(buf_fut);
+
+    ABT_test_printf(1, "ULT %d running on ES %d\n", my_id, rank);
+}
+
+void *pthread_test(void *arg)
+{
+    ABT_xstream xstreams[2];
+    ABT_pool pools[2];
+    ABT_thread threads[2];
+    ABT_task tasks[2];
+    size_t i;
+    int ret;
+
+    /* Execution Streams */
+    for (i = 0; i < 2; i++) {
+        ret = ABT_xstream_create(ABT_SCHED_NULL, &xstreams[i]);
+        ABT_TEST_ERROR(ret, "ABT_xstream_create");
+    }
+
+    /* Create synchronization objects */
+    ret = ABT_mutex_create(&g_mutex);
+    ABT_TEST_ERROR(ret, "ABT_mutex_create");
+    ret = ABT_cond_create(&g_cond);
+    ABT_TEST_ERROR(ret, "ABT_cond_create");
+    for (i = 0; i < 2; i++) {
+        ret = ABT_eventual_create(BUF_SIZE, &g_eventual[i]);
+        ABT_TEST_ERROR(ret, "ABT_eventual_create");
+        ret = ABT_future_create(1, NULL, &g_future[i]);
+        ABT_TEST_ERROR(ret, "ABT_future_create");
+    }
+
+    /* Create ULTs and tasklets */
+    for (i = 0; i < 2; i++) {
+        ret = ABT_xstream_get_main_pools(xstreams[i], 1, &pools[i]);
+        ABT_TEST_ERROR(ret, "ABT_xstream_get_main_pools");
+
+        ret = ABT_thread_create(pools[i], thread_func, (void *)i,
+                                ABT_THREAD_ATTR_NULL, &threads[i]);
+        ABT_TEST_ERROR(ret, "ABT_thread_create");
+
+        ret = ABT_task_create(pools[i], task_func, (void *)i, &tasks[i]);
+        ABT_TEST_ERROR(ret, "ABT_thread_create");
+    }
+
+    /* Test mutex */
+    ABT_mutex_lock(g_mutex);
+    g_counter++;
+    ABT_mutex_unlock(g_mutex);
+
+    /* Test condition variable */
+    /* Wake up other ULTs that are waiting on the condition variable */
+    while (g_threads < 2);
+    ABT_mutex_lock(g_mutex);
+    g_threads = 0;
+    ABT_cond_broadcast(g_cond);
+    ABT_mutex_unlock(g_mutex);
+
+    /* Wait on the condition variable */
+    ABT_mutex_lock(g_mutex);
+    g_threads++;
+    ABT_cond_wait(g_cond, g_mutex);
+    ABT_mutex_unlock(g_mutex);
+
+    /* Test eventual */
+    void *evt_result;
+    char *buf_evt = (char *)malloc(BUF_SIZE);
+    ABT_eventual_set(g_eventual[0], buf_evt, BUF_SIZE);
+    ABT_eventual_wait(g_eventual[1], &evt_result);
+    free(buf_evt);
+
+    /* Test future */
+    char *buf_fut = (char *)malloc(BUF_SIZE);
+    ABT_future_wait(g_future[0]);
+    ABT_future_set(g_future[1], (void *)buf_fut);
+    free(buf_fut);
+
+    /* Join */
+    for (i = 0; i < 2; i++) {
+        ret = ABT_thread_join(threads[i]);
+        ABT_TEST_ERROR(ret, "ABT_thread_join");
+    }
+    for (i = 0; i < 2; i++) {
+        ret = ABT_xstream_join(xstreams[i]);
+        ABT_TEST_ERROR(ret, "ABT_xstream_join");
+    }
+
+    /* Free */
+    ret = ABT_mutex_free(&g_mutex);
+    ABT_TEST_ERROR(ret, "ABT_mutex_free");
+    ret = ABT_cond_free(&g_cond);
+    ABT_TEST_ERROR(ret, "ABT_cond_free");
+    for (i = 0; i < 2; i++) {
+        ret = ABT_eventual_free(&g_eventual[i]);
+        ABT_TEST_ERROR(ret, "ABT_eventual_free");
+        ret = ABT_future_free(&g_future[i]);
+        ABT_TEST_ERROR(ret, "ABT_future_free");
+
+        ret = ABT_thread_free(&threads[i]);
+        ABT_TEST_ERROR(ret, "ABT_thread_free");
+        ret = ABT_task_free(&tasks[i]);
+        ABT_TEST_ERROR(ret, "ABT_task_free");
+        ret = ABT_xstream_free(&xstreams[i]);
+        ABT_TEST_ERROR(ret, "ABT_xstream_free");
+    }
+
+    /* Validation */
+    ABT_test_printf(1, "g_counter=%d\n", g_counter);
+    assert(g_counter == 3);
+
+    pthread_exit(NULL);
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t pthread;
+    int ret;
+
+    /* Initialize */
+    ABT_test_init(argc, argv);
+
+    /* Create a pthread */
+    ret = pthread_create(&pthread, NULL, pthread_test, NULL);
+    assert(ret == 0);
+
+    /* Join the pthread */
+    ret = pthread_join(pthread, NULL);
+    assert(ret == 0);
+
+    /* Finalize */
+    return ABT_test_finalize(0);
+}
+
