95a2a05ab10b5c4b244b21f4531f1be93e712af8
Sangmin Seo <sseo@anl.gov>
2015-04-24 14:57:34 -0500

    fibonacci_future: clean up and modify for new APIs.
    Updated examples/fibonacci_future to use new APIs and multiple ESs.
    
    Signed-off-by: Huiwei Lu <huiweilu@mcs.anl.gov>

--- examples/fibonacci_future.c
+++ examples/fibonacci_future.c
@@ -9,88 +9,143 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <abt.h>
 
-#define N 5
+#define N               10
+#define NUM_XSTREAMS    4
+
+/* global variables */
+ABT_pool g_pool = ABT_POOL_NULL;
 
 /* structure to pass arguments to threads */
 typedef struct {
     int n;
-	int result;
+    int result;
     ABT_future future;
 } thread_args;
 
 /* Callback function passed to future */
-void callback(void **args){
-	int n1,n2;
+void callback(void **args)
+{
+	int n1, n2;
 
-	n1 = ((int *)args[0])[0];
-	n2 = ((int *)args[1])[0];
-	((int *)args[2])[0] = n1+n2;
+	n1 = *(int *)args[1];
+	n2 = *(int *)args[2];
+	*(int *)args[0] = n1 + n2;
 }
 
 /* Function to compute Fibonacci numbers */
-void fibonacci(void *arguments){
+void fibonacci(void *arguments)
+{
     int n;
     thread_args a1, a2;
-    ABT_thread t1, t2;
-    ABT_xstream xstream;
     ABT_future future, fut;
 
-    thread_args *args = (thread_args *) arguments;
+    thread_args *args = (thread_args *)arguments;
     n = args->n;
     future = args->future;
 
     /* checking for base cases */
-    if(n <= 2)
+    if (n <= 2)
         args->result = 1;
     else {
-        ABT_xstream_self(&xstream);
-        ABT_future_create(3,&callback,&fut);
-		ABT_future_set(fut, (void *)&args->result);
-        a1.n = n-1;
+        ABT_future_create(3, callback, &fut);
+        ABT_future_set(fut, (void *)&args->result);
+
+        a1.n = n - 1;
         a1.future = fut;
-        ABT_thread_create(xstream, fibonacci, &a1, ABT_THREAD_ATTR_NULL, &t1);
-        a2.n = n-2;
+        ABT_thread_create(g_pool, fibonacci, &a1, ABT_THREAD_ATTR_NULL, NULL);
+
+        a2.n = n - 2;
         a2.future = fut;
-        ABT_thread_create(xstream, fibonacci, &a2, ABT_THREAD_ATTR_NULL, &t2);
+        ABT_thread_create(g_pool, fibonacci, &a2, ABT_THREAD_ATTR_NULL, NULL);
+
         ABT_future_wait(fut);
-		ABT_future_free(&fut);
+        ABT_future_free(&fut);
     }
 
     /* checking whether to signal the future */
-    if(future != ABT_FUTURE_NULL){
+    if (future != ABT_FUTURE_NULL) {
         ABT_future_set(future, (void *)&args->result);
-    } 
+    }
+}
+
+/* Verification function */
+int verify(int n)
+{
+    int i;
+    int old[2], val;
+
+    if (n <= 2) return 1;
+
+    old[0] = old[1] = 1;
+    for (i = 3; i <= n; i++) {
+        val = old[0] + old[1];
+        old[i % 2] = val;
+    }
+    return val;
 }
 
 /* Main function */
 int main(int argc, char *argv[])
 {
-    int n;
-    ABT_xstream xstream;
+    int n, i, expected;
+    int num_xstreams;
+    ABT_xstream *xstreams;
     ABT_thread thread;
     thread_args args;
 
-    /* init and thread creation */
+    if (argc > 1 && strcmp(argv[1], "-h") == 0) {
+        printf("Usage: %s [N=10] [num_ES=4]\n", argv[0]);
+        return EXIT_SUCCESS;
+    }
+    n = argc > 1 ? atoi(argv[1]) : N;
+    num_xstreams = argc > 2 ? atoi(argv[2]) : NUM_XSTREAMS;
+    printf("# of ESs: %d\n", num_xstreams);
+
+    /* initialization */
     ABT_init(argc, argv);
-    if(argc > 1)
-        n = atoi(argv[1]);
-    else
-        n = N;
+
+    /* shared pool creation */
+    ABT_pool_create_basic(ABT_POOL_FIFO, ABT_POOL_ACCESS_MPMC, ABT_TRUE,
+                          &g_pool);
+
+    /* ES creation */
+    xstreams = (ABT_xstream *)malloc(sizeof(ABT_xstream) * num_xstreams);
+    ABT_xstream_self(&xstreams[0]);
+    ABT_xstream_set_main_sched_basic(xstreams[0], ABT_SCHED_DEFAULT,
+                                     1, &g_pool);
+    for (i = 1; i < num_xstreams; i++) {
+        ABT_xstream_create_basic(ABT_SCHED_DEFAULT, 1, &g_pool,
+                                 ABT_SCHED_CONFIG_NULL, &xstreams[i]);
+        ABT_xstream_start(xstreams[i]);
+    }
+
     args.n = n;
     args.future = ABT_FUTURE_NULL;
-    ABT_xstream_self(&xstream);
-    ABT_thread_create(xstream, fibonacci, &args, ABT_THREAD_ATTR_NULL, &thread);
+    ABT_thread_create(g_pool, fibonacci, &args, ABT_THREAD_ATTR_NULL, &thread);
 
-    /* switch to other user-level threads */
-    ABT_thread_yield();
-
-    /* join other threads */
+    /* join the thread */
     ABT_thread_join(thread);
+    ABT_thread_free(&thread);
 
-	printf("The %d-th number in the Fibonacci sequence is: %d\n",n,args.result);
+    /* join ESs */
+    for (i = 1; i < num_xstreams; i++) {
+        ABT_xstream_join(xstreams[i]);
+        ABT_xstream_free(&xstreams[i]);
+    }
 
     ABT_finalize();
-    return 0;
+
+    free(xstreams);
+
+    printf("Fib(%d): %d\n", n, args.result);
+    expected = verify(n);
+    if (args.result != expected) {
+        fprintf(stderr, "ERROR: expected=%d\n", expected);
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
