236f0a1d3b71ba3ff864089130003c280b3132d3
Sangmin Seo <sseo@anl.gov>
2015-08-12 15:50:10 -0500

    task: add ABTI_task_set/unset_request.
    ABTI_task_set/unset_request are for setting/unsetting a request
    to/from the target tasklet.
    
    No reviewer.

--- src/include/abti_task.h
+++ src/include/abti_task.h
@@ -40,5 +40,17 @@ ABT_task ABTI_task_get_handle(ABTI_task *p_task)
 #endif
 }
 
+static inline
+void ABTI_task_set_request(ABTI_task *p_task, uint32_t req)
+{
+    ABTD_atomic_fetch_or_uint32(&p_task->request, req);
+}
+
+static inline
+void ABTI_task_unset_request(ABTI_task *p_task, uint32_t req)
+{
+    ABTD_atomic_fetch_and_uint32(&p_task->request, ~req);
+}
+
 #endif /* TASK_H_INCLUDED */
 
--- src/task.c
+++ src/task.c
@@ -192,7 +192,7 @@ int ABT_task_cancel(ABT_task task)
     ABTI_CHECK_NULL_TASK_PTR(p_task);
 
     /* Set the cancel request */
-    ABTD_atomic_fetch_or_uint32(&p_task->request, ABTI_TASK_REQ_CANCEL);
+    ABTI_task_set_request(p_task, ABTI_TASK_REQ_CANCEL);
 
   fn_exit:
     return abt_errno;
