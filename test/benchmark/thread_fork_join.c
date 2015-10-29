8ea7b5e469b638a25edada41814aeead00b641a8
Sangmin Seo <sseo@anl.gov>
2015-10-29 17:44:15 +0000

    thread_fork_join: more test cases
    Changes:
     - We create/free a maximum number of ULTs before starting the
       benchmark loop.
     - We build binaries to measure L1/L2 cache misses.
     - For benchmarking ABT_thread_join_many, thread_fork_join_many,
       thread_fork_join_many_papi, and thread_fork_join_many_papi_l1m_l2m
       are built from thread_fork_join.c.
     - To see the benefit of private pools, we build
       thread_fork_join_many_priv_pool from thread_fork_join.c
    
    No reviewer.

--- test/benchmark/thread_fork_join.c
+++ test/benchmark/thread_fork_join.c
@@ -12,7 +12,7 @@
 #include "abt.h"
 #include "abttest.h"
 
-#define START_NULTS 1024     /* Initial test config in terms of #ULTs*/
+#define START_NULTS 64     /* Initial test config in terms of #ULTs*/
 
 /* FIXME: We might want to define cycle counters as ABT_timers
  * and be globally accessible */
@@ -84,7 +84,7 @@ do {                                                                            
         crea_time /= iter; crea_timestd = sqrt(crea_timestd/iter - crea_time*crea_time);            \
         join_time /= iter; join_timestd = sqrt(join_timestd/iter - join_time*join_time);            \
         free_time /= iter; free_timestd = sqrt(free_timestd/iter - free_time*free_time);            \
-        printf("%d \t %d \t %d \t %.2f [%.2f] \t %.2f [%.2f] \t %.2f [%.2f]\n", my_es, nults, iter,   \
+        printf("%-3d %8d %8d %10.2f [%.2f] %10.2f [%.2f] %10.2f [%.2f]\n", my_es, nults, iter,\
                          crea_time, crea_timestd, join_time, join_timestd, free_time, free_timestd);\
        fflush(stdout);                                                                              \
 } while(0)
@@ -103,9 +103,9 @@ do {                                                                            
         free_time /= iter; free_timestd = sqrt(free_timestd/iter - free_time*free_time);            \
         free_llcm /= iter; free_llcmstd = sqrt(free_llcmstd/iter - free_llcm*free_llcm);            \
         free_tlbm /= iter; free_tlbmstd = sqrt(free_tlbmstd/iter - free_tlbm*free_tlbm);            \
-        printf("%d \t %d \t %d \t %.2f [%.2f] \t %.2f [%.2f] \t %.2f [%.2f]"                        \
-                              "\t %.2f [%.2f] \t %.2f [%.2f] \t %.2f [%.2f]"                        \
-                              "\t %.2f [%.2f] \t %.2f [%.2f] \t %.2f [%.2f]\n", my_es, nults, iter, \
+        printf("%-3d %8d %8d %10.2f [%.2f] %6.2f [%.2f] %6.2f [%.2f] "                           \
+                            "%10.2f [%.2f] %6.2f [%.2f] %6.2f [%.2f] "                           \
+                            "%10.2f [%.2f] %6.2f [%.2f] %6.2f [%.2f]\n", my_es, nults, iter,     \
                          crea_time, crea_timestd, crea_llcm, crea_llcmstd, crea_tlbm, crea_tlbmstd, \
                          join_time, join_timestd, join_llcm, join_llcmstd, join_tlbm, join_tlbmstd, \
                          free_time, free_timestd, free_llcm, free_llcmstd, free_tlbm, free_tlbmstd);\
@@ -113,10 +113,19 @@ do {                                                                            
 } while(0)
 #endif
 
+#ifdef USE_PAPI
+ABT_xstream ABTX_xstream_get_self(void) {
+    ABT_xstream self;
+    ABT_xstream_self(&self);
+    return self;
+}
+#endif
+
 
 static ABT_xstream* xstreams;
 static ABT_pool*    pools;
 static int niter, max_ults, ness;
+static ABT_xstream_barrier g_xbarrier = ABT_XSTREAM_BARRIER_NULL;
 
 /* The goal of the following sequence generator is to
  * output a data points spread uniformly on a logarithmic
@@ -167,22 +176,56 @@ static void thread_func(void *arg)
 }
 
 static void thread_xtream_master(void* arg) {
+    int my_es = (int)(size_t) arg;
+
 #ifdef USE_PAPI
     /* Create the Event Set */
     int event_set = PAPI_NULL;
     long_long values[2];
+    if (my_es > 0) {
+        ABTX_papi_assert(PAPI_register_thread());
+    }
     ABTX_papi_assert(PAPI_create_eventset(&event_set));
     /* Add events to monitor */
+#if (defined __MIC__) || (defined __KNC__)
+#ifdef USE_PAPI_L1M_L2M
+    ABTX_papi_assert(PAPI_add_event(event_set, PAPI_L1_DCM));
+    ABTX_papi_assert(PAPI_add_event(event_set, PAPI_L1_ICM));
+#else
+    ABTX_papi_assert(PAPI_add_event(event_set, PAPI_L2_LDM));
+    ABTX_papi_assert(PAPI_add_event(event_set, PAPI_TLB_DM));
+#endif /* USE_PAPI_L1M_L2M */
+#else
+#ifdef USE_PAPI_L1M_L2M
+    ABTX_papi_assert(PAPI_add_event(event_set, PAPI_L1_TCM));
+    ABTX_papi_assert(PAPI_add_event(event_set, PAPI_L2_TCM));
+#else
     ABTX_papi_assert(PAPI_add_event(event_set, PAPI_L3_TCM));
     ABTX_papi_assert(PAPI_add_event(event_set, PAPI_TLB_DM));
+#endif /* USE_PAPI_L1M_L2M */
 #endif
-    int my_es = (int)(size_t) arg;
+#endif /* USE_PAPI */
+
     ABT_thread* my_ults = (ABT_thread*) malloc(max_ults*sizeof(ABT_thread));
+
+    /* warm-up */
+    int t;
+    for(t=0; t<max_ults; t++)
+        ABT_thread_create(pools[my_es], thread_func, NULL, ABT_THREAD_ATTR_NULL, &my_ults[t]);
+#ifndef USE_JOIN_MANY
+    for(t=0; t<max_ults; t++)
+        ABT_thread_join(my_ults[t]);
+#else
+    ABT_thread_join_many(max_ults, my_ults);
+#endif
+    for(t=0; t<max_ults; t++)
+        ABT_thread_free(&my_ults[t]);
 
     int nults;
     seq_state_t state;
     seq_init(&state, 2, START_NULTS/2, START_NULTS/2, 1);
     while((nults = seq_get_next_term(&state)) <= max_ults) {
+        ABT_xstream_barrier_wait(g_xbarrier);
         float crea_time = 0.0, crea_timestd = 0.0, crea_llcm = 0.0, crea_llcmstd = 0.0, crea_tlbm = 0.0, crea_tlbmstd = 0.0;
         float join_time = 0.0, join_timestd = 0.0, join_llcm = 0.0, join_llcmstd = 0.0, join_tlbm = 0.0, join_tlbmstd = 0.0;
         float free_time = 0.0, free_timestd = 0.0, free_llcm = 0.0, free_llcmstd = 0.0, free_tlbm = 0.0, free_tlbmstd = 0.0;
@@ -198,8 +241,12 @@ static void thread_xtream_master(void* arg) {
             ABTX_stop_prof(start_time, nults, crea_time, crea_timestd, event_set, values, crea_llcm, crea_llcmstd, crea_tlbm, crea_tlbmstd);
 
             ABTX_start_prof(start_time, event_set);
+#ifndef USE_JOIN_MANY
             for(t=0; t<nults; t++)
                 ABT_thread_join(my_ults[t]);
+#else
+            ABT_thread_join_many(nults, my_ults);
+#endif
             ABTX_stop_prof(start_time, nults, join_time, join_timestd, event_set, values, join_llcm, join_llcmstd, join_tlbm, join_tlbmstd);
 
             ABTX_start_prof(start_time, event_set);
@@ -207,12 +254,18 @@ static void thread_xtream_master(void* arg) {
                 ABT_thread_free(&my_ults[t]);
             ABTX_stop_prof(start_time, nults, free_time, free_timestd, event_set, values, free_llcm, free_llcmstd, free_tlbm, free_tlbmstd);
         }
+        ABT_xstream_barrier_wait(g_xbarrier);
         ABTX_prof_summary(my_es, nults, iter, crea_time, crea_timestd, crea_llcm, crea_llcmstd, crea_tlbm, crea_tlbmstd,
                                               join_time, join_timestd, join_llcm, join_llcmstd, join_tlbm, join_tlbmstd,
                                               free_time, free_timestd, free_llcm, free_llcmstd, free_tlbm, free_tlbmstd);
     }
 
     free(my_ults);
+#ifdef USE_PAPI
+    if (my_es > 0) {
+        ABTX_papi_assert(PAPI_unregister_thread());
+    }
+#endif
 }
 
 int main(int argc, char *argv[])
@@ -224,6 +277,7 @@ int main(int argc, char *argv[])
         fprintf(stderr, "PAPI library init error!\n");
         exit(1);
     }
+    ABTX_papi_assert(PAPI_thread_init(ABTX_xstream_get_self()));
 #endif
 
     int i;
@@ -238,16 +292,54 @@ int main(int argc, char *argv[])
     ABT_init(argc, argv);
 
     /* output beginning */
-    int line_size = 75, nults;
-    ABT_test_print_line(stdout, '-', line_size);
 #ifndef USE_PAPI
-    printf("# ES \t #ULTS \t #Iter \t Create: time [std] \t  Join: time [std] \t Free: time [std]\n");
+    int line_size = 84, nults;
+    ABT_test_print_line(stdout, '-', line_size);
+    printf("%-3s %8s %8s %20s %20s %20s\n",
+            "ES#", "#ULTS", "#Iter",
+            "Create: time [std]",
+            "Join: time [std]",
+            "Free: time [std]");
 #else
-    printf("# ES \t #ULTS \t #Iter \t Create: time [std] LLCm [std] TLBm [std] \t  Join: time [std]  LLCm [std] TLBm [std] \t Free: time [std] LLCm [std] TLBm [std]\n");
+    int line_size = 174, nults;
+    ABT_test_print_line(stdout, '-', line_size);
+#if (defined __MIC__) || (defined __KNC__)
+#ifdef USE_PAPI_L1M_L2M
+    printf("%-3s %8s %8s %20s %14s %14s %20s %14s %14s %20s %14s %14s\n",
+           "ES#", "#ULTS", "#Iter",
+           "Create: time [std]", "L1Dm [std]", "L1Im [std]",
+           "Join: time [std]", "L1Dm [std]", "L1Im [std]",
+           "Free: time [std]", "L1Dm [std]", "L1Im [std]");
+#else
+    printf("%-3s %8s %8s %20s %14s %14s %20s %14s %14s %20s %14s %14s\n",
+           "ES#", "#ULTS", "#Iter",
+           "Create: time [std]", "L2Dm [std]", "TLBm [std]",
+           "Join: time [std]", "L2Dm [std]", "TLBm [std]",
+           "Free: time [std]", "L2Dm [std]", "TLBm [std]");
+#endif /* USE_PAPI_L1M_L2M */
+#else
+#ifdef USE_PAPI_L1M_L2M
+    printf("%-3s %8s %8s %20s %14s %14s %20s %14s %14s %20s %14s %14s\n",
+           "ES#", "#ULTS", "#Iter",
+           "Create: time [std]", "L1Cm [std]", "L2Cm [std]",
+           "Join: time [std]", "L1Cm [std]", "L2Cm [std]",
+           "Free: time [std]", "L1Cm [std]", "L2Cm [std]");
+#else
+    printf("%-3s %8s %8s %20s %14s %14s %20s %14s %14s %20s %14s %14s\n",
+           "ES#", "#ULTS", "#Iter",
+           "Create: time [std]", "LLCm [std]", "TLBm [std]",
+           "Join: time [std]", "LLCm [std]", "TLBm [std]",
+           "Free: time [std]", "LLCm [std]", "TLBm [std]");
+#endif /* USE_PAPI_L1M_L2M */
 #endif
+#endif /* USE_PAPI */
 
     ABT_test_print_line(stdout, '-', line_size);
 
+    /* create a global barrier */
+    ABT_xstream_barrier_create(ness, &g_xbarrier);
+
+#ifndef USE_PRIV_POOL
     /* Create ESs*/
     ABT_xstream_self(&xstreams[0]);
     for(i=1; i<ness; i++) {
@@ -260,11 +352,31 @@ int main(int argc, char *argv[])
 
     ABT_xstream_get_main_pools(xstreams[0], 1, &pools[0]);
     thread_xtream_master((void*)(size_t)0);
+#else
+    /* Create pools */
+    for(i=0; i<ness; i++) {
+        ABT_pool_create_basic(ABT_POOL_FIFO, ABT_POOL_ACCESS_PRIV, ABT_TRUE, &pools[i]);
+    }
+
+    for(i=1; i<ness; i++) {
+        ABT_thread_create(pools[i], thread_xtream_master, (void*)(size_t)i, ABT_THREAD_ATTR_NULL, NULL);
+    }
+
+    /* Create ESs*/
+    ABT_xstream_self(&xstreams[0]);
+    ABT_xstream_set_main_sched_basic(xstreams[0], ABT_SCHED_DEFAULT, 1, &pools[0]);
+    for(i=1; i<ness; i++) {
+        ABT_xstream_create_basic(ABT_SCHED_DEFAULT, 1, &pools[i], ABT_SCHED_CONFIG_NULL, &xstreams[i]);
+    }
+
+    thread_xtream_master((void*)(size_t)0);
+#endif
 
     for(i=1; i<ness; i++) {
         ABT_xstream_join(xstreams[i]);
         ABT_xstream_free(&xstreams[i]);
     }
+    ABT_xstream_barrier_free(&g_xbarrier);
 
     ABT_finalize();
 
