bc3b59d73eaf300f80b0ee0d647072e14e11e2bf
Sangmin Seo <sseo@anl.gov>
2016-03-22 14:19:18 -0500

    thread: resolve the empty body warning.
    clang generates a warning when while loop has empty body.  This patch
    resolves the warning issue.
    
    No reviewer.

--- src/thread.c
+++ src/thread.c
@@ -1764,7 +1764,8 @@ int ABTI_thread_set_ready(ABTI_thread *p_thread)
     /* We should wait until the scheduler of the blocked ULT resets the BLOCK
      * request. Otherwise, the ULT can be pushed to a pool here and be
      * scheduled by another scheduler if it is pushed to a shared pool. */
-    while (*(volatile uint32_t *)(&p_thread->request) & ABTI_THREAD_REQ_BLOCK);
+    while (*(volatile uint32_t *)(&p_thread->request) & ABTI_THREAD_REQ_BLOCK) {
+    }
 
     LOG_EVENT("[U%" PRIu64 ":E%" PRIu64 "] set ready\n",
               ABTI_thread_get_id(p_thread), p_thread->p_last_xstream->rank);
