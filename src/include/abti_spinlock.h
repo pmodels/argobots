dc976b832149e894d2519b3f406e07c438d272f8
Shintaro Iwasaki <siwasaki@anl.gov>
2018-01-30 11:08:03 -0600

    Fix a spinlock implementation.
    The previous spinlock issues mem_barrier "after" releasing locks.
    It means that, any memory accesses before membarrier can be
    reordered, so some memory writes might happen (although it is
    compiler dependent) after releasing a lock.  Other threads can
    take that lock and read that memory, as far as my understanding.
    
    Because locks are used to ensure all data accesses while taking a
    lock are done in the locked section, mem_barrier must be placed
    "before" releasing locks.

--- src/include/abti_spinlock.h
+++ src/include/abti_spinlock.h
@@ -30,8 +30,8 @@ static inline void ABTI_spinlock_acquire(ABTI_spinlock *p_lock)
 
 static inline void ABTI_spinlock_release(ABTI_spinlock *p_lock)
 {
-    *(volatile uint32_t *)&p_lock->val = 0;
     ABTD_atomic_mem_barrier();
+    *(volatile uint32_t *)&p_lock->val = 0;
 }
 
 #endif /* SPINLOCK_H_INCLUDED */
