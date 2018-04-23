fd8c4a4c4ebc93b15d007d17faa82eefa6021131
Phil Carns <carns@mcs.anl.gov>
2018-04-23 14:47:46 -0400

    pool: add pop_wait() and pop_timedwait() fns
    Signed-off-by: Halim Amer <aamer@anl.gov>

--- src/pool/fifo.c
+++ src/pool/fifo.c
@@ -14,6 +14,8 @@ static void     pool_push_shared(ABT_pool pool, ABT_unit unit);
 static void     pool_push_private(ABT_pool pool, ABT_unit unit);
 static ABT_unit pool_pop_shared(ABT_pool pool);
 static ABT_unit pool_pop_private(ABT_pool pool);
+static ABT_unit pool_pop_wait(ABT_pool pool);
+static ABT_unit pool_pop_timedwait(ABT_pool pool, double abstime_secs);
 static int      pool_remove_shared(ABT_pool pool, ABT_unit unit);
 static int      pool_remove_private(ABT_pool pool, ABT_unit unit);
 
@@ -72,6 +74,8 @@ int ABTI_pool_get_fifo_def(ABT_pool_access access, ABT_pool_def *p_def)
     p_def->p_init               = pool_init;
     p_def->p_free               = pool_free;
     p_def->p_get_size           = pool_get_size;
+    p_def->p_pop_wait           = pool_pop_wait;
+    p_def->p_pop_timedwait      = pool_pop_timedwait;
     p_def->u_get_type           = unit_get_type;
     p_def->u_get_thread         = unit_get_thread;
     p_def->u_get_task           = unit_get_task;
@@ -193,6 +197,18 @@ static void pool_push_private(ABT_pool pool, ABT_unit unit)
     p_data->num_units++;
 
     p_unit->pool = pool;
+}
+
+static ABT_unit pool_pop_wait(ABT_pool pool)
+{
+    HANDLE_ERROR("ABT_POOL_FIFO does not support pop_wait operation");
+    ABTI_ASSERT(0);
+}
+
+static ABT_unit pool_pop_timedwait(ABT_pool pool, double abstime_secs)
+{
+    HANDLE_ERROR("ABT_POOL_FIFO does not support pop_timedwait operation");
+    ABTI_ASSERT(0);
 }
 
 static ABT_unit pool_pop_shared(ABT_pool pool)
