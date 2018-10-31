e6e43cd7d2ce4d0da4e76d2c0ba5ff43dc31f84c
Shintaro Iwasaki <siwasaki@anl.gov>
2018-10-30 19:18:56 -0500

    thread: fix ABTI_LOG_SET_SCHED argument in ABTDI_thread_terminate.
    In ABTDI_thread_terminate, the caller must update the current scheduler whenever
    a ULT jumps to its parent scheduler. This commit removes the condition and
    always sets lp_ABTI_log->p_sched to the parent scheduler.

--- src/arch/abtd_thread.c
+++ src/arch/abtd_thread.c
@@ -149,9 +149,7 @@ static inline void ABTDI_thread_terminate(ABTI_thread *p_thread,
     } else {
 #endif
         p_sched = ABTI_xstream_get_top_sched(p_thread->p_last_xstream);
-        ABTI_LOG_SET_SCHED((p_sched->p_ctx == p_link)
-                           ? ABTI_xstream_get_top_sched(p_thread->p_last_xstream)
-                           : NULL);
+        ABTI_LOG_SET_SCHED(ABTI_xstream_get_top_sched(p_thread->p_last_xstream));
 #ifndef ABT_CONFIG_DISABLE_STACKABLE_SCHED
     }
 #endif
