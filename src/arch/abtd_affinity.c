011dafea8f7dc61ca5ecf17209c6d44918f51979
Shintaro Iwasaki <siwasaki@anl.gov>
2020-08-11 14:47:48 -0500

    affinity: add comments on the format of affinity parameter

--- src/arch/abtd_affinity.c
+++ src/arch/abtd_affinity.c
@@ -3,6 +3,165 @@
  * See COPYRIGHT in top-level directory.
  */
 
+/*
+ * The Argobots affinity module parses the following grammar while it ignores
+ * all white spaces between tokens.  Double-quoted symbols in the following are
+ * part of the syntax.
+ *
+ * <list>         = <interval>
+ *                | <list> "," <interval>
+ * <interval>     = <es-id-list> ":" <num> ":" <stride>
+ *                | <es-id-list> ":" <num>
+ *                | <es-id-list>
+ * <es-id-list>   = <id>
+ *                | "{" <id-list> "}"
+ * <id-list>      = <id-interval>
+ *                | <id-list> "," <id-interval>
+ * <id-interval>  = <id> ":" <num> ":" <stride>
+ *                | <id> ":" <num>
+ *                | <id>
+ * <id>           = <integer>
+ * <stride>       = <integer>
+ * <num>          = <positive integer>
+ *
+ * An execution stream with rank n refers to the (n % N)th CPU ID list
+ * (<es-id-list>) of the whole list (<list>) that has N items.  The execution
+ * stream will be scheduled on a core that has a CPU ID in its CPU ID list
+ * (<es-id-list>).  If a CPU ID is smaller than zero or larger than the number
+ * of cores recognized by the system, a modulo of the number of cores is used.
+ *
+ * This grammar supports a pattern-based syntax.  <id-interval> can represent
+ * multiple CPU IDs by specifying a base CPU ID (<id>), the number of CPU IDs
+ * (<num>), and a stride (<stride>) as follows:
+ *   <id>, <id> + <stride>, ..., <id> + <stride> * (<num> - 1)
+ * <interval> also accepts a pattern-based syntax as follows:
+ *   <es-id-list>,
+ *   {<es-id-list>[0] + <stride>, <es-id-list>[1] + <stride>, ...}, ...
+ *   {<es-id-list>[0] + <stride> * (<num> - 1),
+ *    <es-id-list>[1] + <stride> * (<num> - 1), ...}
+ * Note that <num> and <stride> are set to 1 if they are omitted.
+ *
+ * Let us assume a 12-core machine (NOTE: "12-core" does not mean 12 physical
+ * cores but indicates 12 hardware threads on modern CPUs).  Examples are as
+ * follows:
+ *
+ * Example 1: bind ES with rank n to a core that has CPU ID = n % 12
+ *
+ * | ES0 | ES1 | ES2 | ES3 | ES4 | ES5 | ES6 | ES7 | ES8 | ES9 | ES10| ES11|
+ * |CPU00|CPU01|CPU02|CPU03|CPU04|CPU05|CPU06|CPU07|CPU08|CPU09|CPU10|CPU11|
+ *
+ * (1.1) ABT_SET_AFFINITY="{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}"
+ *      This explicitly sets all the CPU IDs.
+ * (1.2) ABT_SET_AFFINITY="0,1,2,3,4,5,6,7,8,9,10,11"
+ *      It is similar to (1.1), but it omits "{}" since "{id}" = "id".
+ * (1.3) ABT_SET_AFFINITY="{0}:12:1"
+ *      This creates 12 CPU ID lists starting from {0}.  The stride is specified
+ *      as 1, so the created lists are {0}, {1}, {2}, ... {11}.
+ * (1.4) ABT_SET_AFFINITY="{0}:12"
+ *      The default stride is 1, so it is the same as (1.3)
+ * (1.5) ABT_SET_AFFINITY="0:12"
+ *      It omits "{}" in (1.4) since "{id}" = "id".
+ *
+ * Example 2: bind ES with rank n to a core that has CPU ID = (n + 6) % 12
+ *
+ * | ES6 | ES7 | ES8 | ES9 | ES10| ES11| ES0 | ES1 | ES2 | ES3 | ES4 | ES5 |
+ * |CPU00|CPU01|CPU02|CPU03|CPU04|CPU05|CPU06|CPU07|CPU08|CPU09|CPU10|CPU11|
+ *
+ * (2.1) ABT_SET_AFFINITY="{6},{7},{8},{9},{10},{11},{0},{1},{2},{3},{4},{5}"
+ *      This explicitly sets all the CPU IDs.
+ * (2.2) ABT_SET_AFFINITY="6,7,8,9,10,11,0,1,2,3,4,5"
+ *      It is similar to (2.1), but it omits "{}" since "{id}" = "id".
+ * (2.3) ABT_SET_AFFINITY="{6}:6:1,{0}:6:1"
+ *      This first creates 6 CPU ID lists starting from {6} and then 6 lists
+ *      starting from {0}.  The stride is "1", so the created lists are
+ *      {6}, ... {11}, {0} ... {5}.
+ * (2.4) ABT_SET_AFFINITY="{6}:6,{0}:6"
+ *      The default stride is 1, so it is the same as (2.3)
+ * (2.5) ABT_SET_AFFINITY="6:6,0:6"
+ *      It omits "{}" in (2.4) since "{id}" = "id".
+ * (2.6) ABT_SET_AFFINITY="6:12"
+ *      Affinity setting wraps around with respect to the number of cores, so
+ *      this works the same as (2.5) on a 12-core machine.
+ *
+ * Example 3: bind ES with rank n to core that has CPU ID = (n * 4) % 12
+ *
+ * | ES0 |     |     |     | ES1 |     |     |     | ES2 |     |     |     |
+ * |CPU00|CPU01|CPU02|CPU03|CPU04|CPU05|CPU06|CPU07|CPU08|CPU09|CPU10|CPU11|
+ *
+ * (3.1) ABT_SET_AFFINITY="{0},{4},{8}"
+ *      This explicitly sets all the CPU IDs.
+ * (3.2) ABT_SET_AFFINITY="0,4,8"
+ *      It is similar to (3.1), but it omits "{}" since "{id}" = "id".
+ * (3.3) ABT_SET_AFFINITY="{0}:3:4"
+ *      This creates 3 CPU ID lists starting from {0}. The stride is "4", so
+ *      creates lists are {0}, {4}, {8}.
+ * (3.4) ABT_SET_AFFINITY="0:3:4"
+ *      It omits "{}" in (3.3) since "{id}" = "id".
+ *
+ * Example 4: bind ES with rank n to core that has
+ *            CPU ID = (n * 4 + (n % 12) / 3) % 12
+ *
+ * | ES0 | ES3 | ES6 | ES9 | ES1 | ES4 | ES7 | ES10| ES2 | ES5 | ES9 | ES11|
+ * |CPU00|CPU01|CPU02|CPU03|CPU04|CPU05|CPU06|CPU07|CPU08|CPU09|CPU10|CPU11|
+ *
+ * (4.1) ABT_SET_AFFINITY="{0},{4},{8},{1},{5},{9},{2},{6},{10},{3},{7},{11}"
+ *      This explicitly sets all the CPU IDs.
+ * (4.2) ABT_SET_AFFINITY="0,4,8,1,5,9,2,6,10,3,7,11"
+ *      It is similar to (4.1), but it omits "{}" since "{id}" = "id".
+ * (4.3) ABT_SET_AFFINITY="{0}:3:4,{1}:3:4,{2}:3:4,{3}:3:4"
+ *      This creates 3 CPU ID lists ({0}, {4}, {8}), then ({1}, {5}, {9}),
+ *      ({2}, {6}, {10}), and ({3}, {7}, {11}).
+ * (4.4) ABT_SET_AFFINITY="0:3:4,1:3:4,2:3:4,3:3:4"
+ *      It omits "{}" in (4.3) since "{id}" = "id".
+ *
+ * Example 5: bind ES with rank n to cores that have
+ *            (n * 4) % 12 <= CPU ID < (n * 4) % 12 + 4
+ *
+ * |          ES0          |          ES1          |          ES2          |
+ * |CPU00|CPU01|CPU02|CPU03|CPU04|CPU05|CPU06|CPU07|CPU08|CPU09|CPU10|CPU11|
+ *
+ * (5.1) ABT_SET_AFFINITY="{0,1,2,3},{4,5,6,7},{8,9,10,11}"
+ *      This explicitly sets all the CPU IDs.
+ * (5.2) ABT_SET_AFFINITY="{0,1,2,3}:3:4"
+ *      This creates 3 CPU ID lists starting from {0,1,2,3}. The stride is "4",
+ *      so the created lists are "{0,1,2,3}, {4,5,6,7}, {8,9,10,11}".
+ *      Note that "{0,1,2,3}:3:1" (or "{0,1,2,3}:3") is wrong: they create
+ *      "{0,1,2,3}, {2,3,4,5}, {3,4,5,6}" since the stride is 1.
+ * (5.3) ABT_SET_AFFINITY="{0:4:1}:3:4"
+ *      "{0:4:1}" means a CPU ID list of 4 CPU IDs that starts from 0 with a
+ *      stride 1, so it is the same as "{0,1,2,3}".
+ * (5.4) ABT_SET_AFFINITY="{0:4}:3:4"
+ *      The default stride is 1.
+ *
+ * Example 6: bind ES with rank n to cores that have either of the following:
+ *            CPU ID = (n * 2) % 6
+ *            CPU ID = (n * 2) % 6 + 6
+ *
+ * |    ES0    |    ES1    |    ES2    |    ES3    |    ES4    |    ES5    |
+ * |CPU00|CPU06|CPU01|CPU07|CPU02|CPU08|CPU03|CPU09|CPU04|CPU10|CPU05|CPU11|
+ *
+ * (6.1) ABT_SET_AFFINITY="{0,6},{1,7},{2,8},{3,9},{4,10},{5,11}"
+ *      This explicitly sets all the CPU IDs.
+ * (6.2) ABT_SET_AFFINITY="{0,6}:6:1"
+ *      This creates 6 CPU ID lists starting from {0,6}. The stride is "1", so
+ *      the created lists are "{0,6}, {1,7}, {2,8}, {3,9}, {4,10}, {5,11}".
+ * (6.3) ABT_SET_AFFINITY="{0,6}:6"
+ *      The default stride is 1.
+ *
+ * Example 7: bind ESs to cores except for those that have CPU ID = 0 or 1
+ *
+ * |           |                    ES0, ES1, ES2, ...                     |
+ * |CPU00|CPU01|CPU02|CPU03|CPU04|CPU05|CPU06|CPU07|CPU08|CPU09|CPU10|CPU11|
+ *
+ * (7.1) ABT_SET_AFFINITY="{2,3,4,5,6,7,8,9,10,11}"
+ *      This explicitly sets all the CPU IDs.
+ * (7.2) ABT_SET_AFFINITY="{2:10:1}"
+ *      "{2:10:1}" means a CPU ID list that has 10 CPU IDs starting from 2 with
+ *      a stride 1, so it is the same as "{2,3,4,5,6,7,8,9,10,11}".
+ * (7.3) ABT_SET_AFFINITY="{2:10}"
+ *      The default stride is 1.
+ */
+
 #include "abti.h"
 #include <unistd.h>
 
