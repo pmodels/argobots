0ba7844944a1ac3dd8902294441e4bf764dadd84
Shintaro Iwasaki <siwasaki@anl.gov>
2020-08-31 11:29:27 -0500

    stream_barrier: fix error handling

--- src/include/abtd_stream.h
+++ src/include/abtd_stream.h
@@ -14,15 +14,16 @@ static inline int ABTD_xstream_barrier_init(uint32_t num_waiters,
     return (ret == 0) ? ABT_SUCCESS : ABT_ERR_XSTREAM_BARRIER;
 }
 
-static inline int ABTD_xstream_barrier_destroy(ABTD_xstream_barrier *p_barrier)
+static inline void ABTD_xstream_barrier_destroy(ABTD_xstream_barrier *p_barrier)
 {
     int ret = pthread_barrier_destroy(p_barrier);
-    return (ret == 0) ? ABT_SUCCESS : ABT_ERR_XSTREAM_BARRIER;
+    ABTI_ASSERT(ret == 0);
 }
 
-static inline int ABTD_xstream_barrier_wait(ABTD_xstream_barrier *p_barrier)
+static inline void ABTD_xstream_barrier_wait(ABTD_xstream_barrier *p_barrier)
 {
-    return pthread_barrier_wait(p_barrier);
+    int ret = pthread_barrier_wait(p_barrier);
+    ABTI_ASSERT(ret == PTHREAD_BARRIER_SERIAL_THREAD || ret == 0);
 }
 #endif
 
