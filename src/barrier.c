7e4d53a4dd1ded81d589382978dfb487779ac907
Shintaro Iwasaki <siwasaki@anl.gov>
2020-12-23 00:07:34 -0600

    barrier: clean up the specification of the barrier API

--- src/barrier.c
+++ src/barrier.c
@@ -14,14 +14,35 @@
  * @brief   Create a new barrier.
  *
  * \c ABT_barrier_create() creates a new barrier and returns its handle through
- * \c newbarrier.
- * If an error occurs in this routine, a non-zero error code will be returned
- * and \c newbarrier will be set to \c ABT_BARRIER_NULL.
+ * \c newbarrier.  \c num_waiters specifies the number of waiters that must call
+ * \c ABT_barrier_wait() before any of the waiters successfully return from the
+ * call.  \c num_waiters must be greater than zero.
+ *
+ * \c newbarrier must be freed by \c ABT_barrier_free() after its use.
+ *
+ * @changev11
+ * \DOC_DESC_V10_BARRIER_NUM_WAITERS{\c num_waiters}
+ * @endchangev11
+ *
+ * @changev20
+ * \DOC_DESC_V1X_SET_VALUE_ON_ERROR{\c newbarrier, \c ABT_BARRIER_NULL}
+ * @endchangev20
+ *
+ * @contexts
+ * \DOC_CONTEXT_INIT \DOC_CONTEXT_NOCTXSWITCH
+ *
+ * @errors
+ * \DOC_ERROR_SUCCESS
+ * \DOC_ERROR_INV_ARG_ZERO{\c num_waiters}
+ * \DOC_ERROR_RESOURCE
+ *
+ * @undefined
+ * \DOC_UNDEFINED_UNINIT
+ * \DOC_UNDEFINED_NULL_PTR{\c newbarrier}
  *
  * @param[in]  num_waiters  number of waiters
- * @param[out] newbarrier   handle to a new barrier
+ * @param[out] newbarrier   barrier handle
  * @return Error code
- * @retval ABT_SUCCESS on success
  */
 int ABT_barrier_create(uint32_t num_waiters, ABT_barrier *newbarrier)
 {
@@ -43,16 +64,32 @@ int ABT_barrier_create(uint32_t num_waiters, ABT_barrier *newbarrier)
 
 /**
  * @ingroup BARRIER
- * @brief   Reinitialize the barrier.
+ * @brief   Reinitialize a barrier with a new number of waiters.
  *
- * \c ABT_barrier_reinit() reinitializes the barrier \c barrier with a new
- * number of waiters \c num_waiters.  \c num_waiters can be the same as or
- * different from the one passed to \c ABT_barrier_create().
+ * \c ABT_barrier_reinit() reinitializes the barrier \c barrier with the new
+ * number of waiters \c num_waiters.  \c num_waiters must be greater than zero.
  *
- * @param[in] barrier      handle to the barrier
+ * @changev11
+ * \DOC_DESC_V10_BARRIER_NUM_WAITERS{\c num_waiters}
+ * @endchangev11
+ *
+ * @contexts
+ * \DOC_CONTEXT_INIT \DOC_CONTEXT_NOCTXSWITCH
+ *
+ * @errors
+ * \DOC_ERROR_SUCCESS
+ * \DOC_ERROR_INV_ARG_ZERO{\c num_waiters}
+ * \DOC_ERROR_INV_BARRIER_HANDLE{\c barrier}
+ * \DOC_ERROR_RESOURCE
+ *
+ * @undefined
+ * \DOC_UNDEFINED_UNINIT
+ * \DOC_UNDEFINED_WAITER{\c barrier}
+ * \DOC_UNDEFINED_THREAD_UNSAFE{\c barrier}
+ *
+ * @param[in] barrier      barrier handle
  * @param[in] num_waiters  number of waiters
  * @return Error code
- * @retval ABT_SUCCESS on success
  */
 int ABT_barrier_reinit(ABT_barrier barrier, uint32_t num_waiters)
 {
@@ -72,15 +109,26 @@ int ABT_barrier_reinit(ABT_barrier barrier, uint32_t num_waiters)
 
 /**
  * @ingroup BARRIER
- * @brief   Free the barrier.
+ * @brief   Free a barrier.
  *
- * \c ABT_barrier_free() deallocates the memory used for the barrier object
- * associated with the handle \c barrier. If it is successfully processed,
- * \c barrier is set to \c ABT_BARRIER_NULL.
+ * \c ABT_barrier_free() deallocates the resource used for the barrier
+ * \c barrier and sets \c barrier to \c ABT_BARRIER_NULL.
  *
- * @param[in,out] barrier  handle to the barrier
+ * @contexts
+ * \DOC_CONTEXT_INIT \DOC_CONTEXT_NOCTXSWITCH
+ *
+ * @errors
+ * \DOC_ERROR_SUCCESS
+ * \DOC_ERROR_INV_BARRIER_PTR{\c barrier}
+ *
+ * @undefined
+ * \DOC_UNDEFINED_UNINIT
+ * \DOC_UNDEFINED_NULL_PTR{\c barrier}
+ * \DOC_UNDEFINED_WAITER{\c barrier}
+ * \DOC_UNDEFINED_THREAD_UNSAFE_FREE{\c barrier}
+ *
+ * @param[in,out] barrier  barrier handle
  * @return Error code
- * @retval ABT_SUCCESS on success
  */
 int ABT_barrier_free(ABT_barrier *barrier)
 {
@@ -105,14 +153,30 @@ int ABT_barrier_free(ABT_barrier *barrier)
 
 /**
  * @ingroup BARRIER
- * @brief   Wait on the barrier.
+ * @brief   Wait on a barrier.
  *
- * The ULT calling \c ABT_barrier_wait() waits on the barrier until all the
- * ULTs reach the barrier.
+ * The caller of \c ABT_barrier_wait() waits on the barrier \c barrier.  The
+ * caller suspends until as many waiters as the number of waiters specified by
+ * \c ABT_barrier_create() or \c ABT_barrier_reinit() reach \c barrier.
  *
- * @param[in] barrier  handle to the barrier
+ * @changev20
+ * \DOC_DESC_V1X_NOTASK{\c ABT_ERR_BARRIER}
+ * @endchangev20
+ *
+ * @contexts
+ * \DOC_V1X \DOC_CONTEXT_INIT_NOTASK \DOC_CONTEXT_CTXSWITCH\n
+ * \DOC_V20 \DOC_CONTEXT_INIT \DOC_CONTEXT_CTXSWITCH
+ *
+ * @errors
+ * \DOC_ERROR_SUCCESS
+ * \DOC_ERROR_INV_BARRIER_HANDLE{\c barrier}
+ * \DOC_V1X \DOC_ERROR_TASK{\c ABT_ERR_BARRIER}
+ *
+ * @undefined
+ * \DOC_UNDEFINED_UNINIT
+ *
+ * @param[in] barrier  barrier handle
  * @return Error code
- * @retval ABT_SUCCESS on success
  */
 int ABT_barrier_wait(ABT_barrier barrier)
 {
@@ -142,16 +206,26 @@ int ABT_barrier_wait(ABT_barrier barrier)
 
 /**
  * @ingroup BARRIER
- * @brief   Get the number of waiters for the barrier.
+ * @brief   Get the number of waiters of a barrier.
  *
- * \c ABT_barrier_get_num_waiters() returns the number of waiters, which was
- * passed to \c ABT_barrier_create() or \c ABT_barrier_reinit(), for the given
- * barrier \c barrier.
+ * \c ABT_barrier_get_num_waiters() returns the number of waiters of the barrier
+ * \c barrier through \c num_waiters.  The number of waiters is set by
+ * \c ABT_barrier_create() and can be updated by \c ABT_barrier_reinit().
+ *
+ * @contexts
+ * \DOC_CONTEXT_INIT \DOC_CONTEXT_NOCTXSWITCH
+ *
+ * @errors
+ * \DOC_ERROR_SUCCESS
+ * \DOC_ERROR_INV_BARRIER_HANDLE{\c barrier}
+ *
+ * @undefined
+ * \DOC_UNDEFINED_UNINIT
+ * \DOC_UNDEFINED_NULL_PTR{\c num_waiters}
  *
  * @param[in]  barrier      handle to the barrier
  * @param[out] num_waiters  number of waiters
  * @return Error code
- * @retval ABT_SUCCESS on success
  */
 int ABT_barrier_get_num_waiters(ABT_barrier barrier, uint32_t *num_waiters)
 {
