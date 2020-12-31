ca9fa7c9e2612160358bd54143e22453a5320d38
Shintaro Iwasaki <siwasaki@anl.gov>
2020-12-31 13:44:24 -0600

    error: clean up the specification of the error API

--- src/error.c
+++ src/error.c
@@ -6,23 +6,39 @@
 #include "abti.h"
 
 /** @defgroup ERROR Error
- * This group is for error classes.
+ * This group is for Error.
  */
 
 /**
  * @ingroup ERROR
- * @brief   Get the string of error code and its length.
+ * @brief   Retrieve a string of an error code and its length.
  *
- * \c ABT_error_get_str returns the string of given error code through
- * \c str and its length in bytes via \c len. If \c str is NULL, only \c len
- * is returned. If \c str is not NULL, it should have enough space to save
- * \c len bytes of characters. If \c len is NULL, \c len is ignored.
+ * \c ABT_error_get_str() returns a zero-terminated string of the error code
+ * \c err through \c str and its length in bytes through \c len.
  *
- * @param[in]  err    error code
- * @param[out] str    error string
- * @param[out] len    the length of string in bytes
+ * If \c str is not \c NULL, the user must be responsible for passing \c str
+ * that has enough space to save <i>\"the length of the string \+ 1\"</i> bytes
+ * of characters.  If \c str is \c NULL, \c str is not updated.
+ *
+ * If \c len is not \c NULL, this routine sets the length of the string to
+ * \c len.  If \c len is \c NULL, \c len is not updated.
+ *
+ * @changev20
+ * \DOC_DESC_V1X_ERROR_CODE_CHANGE{\c ABT_ERR_OTHER, \c ABT_ERR_INV_ARG,
+ *                                 \c err is not a valid error code}
+ * @endchangev20
+ *
+ * @contexts
+ * \DOC_CONTEXT_ANY \DOC_CONTEXT_NOCTXSWITCH
+ *
+ * @errors
+ * \DOC_V1X \DOC_ERROR_OTHER_ERR_CODE{\c err}
+ * \DOC_V20 \DOC_ERROR_INV_ARG_ERR_CODE{\c err}
+ *
+ * @param[in]  err  error code
+ * @param[out] str  error string
+ * @param[out] len  length of string in bytes
  * @return Error code
- * @retval ABT_SUCCESS on success
  */
 int ABT_error_get_str(int err, char *str, size_t *len)
 {
