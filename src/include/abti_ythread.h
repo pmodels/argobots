cb6f9e132506eec03f6d48fbbec9ebf4b9b1e2ff
Shintaro Iwasaki <siwasaki@anl.gov>
2021-07-13 17:01:00 -0500

    ythread: clean up joiner handling on ULT termination
    This patch adds ABTI_ythread_atomic_get_joiner() and
    ABTI_ythread_resume_joiner() to remove duplication of logic.
    
    This patch fixes joiner release algorithms of ABT_self_exit() and
    cancellation; previously, they do not handle a non-yieldable joiner.
    This causes a bug when a ULT joined by a non-yieldable thread finishes
    by cancellation or ABT_self_exit(_to)().

--- src/include/abti_ythread.h
+++ src/include/abti_ythread.h
@@ -274,6 +274,55 @@ static inline void ABTI_ythread_suspend_to(ABTI_xstream **pp_local_xstream,
 
 void ABTI_ythread_callback_terminate(void *arg);
 
+static inline ABTI_ythread *
+ABTI_ythread_atomic_get_joiner(ABTI_ythread *p_ythread)
+{
+    ABTD_ythread_context *p_ctx = &p_ythread->ctx;
+    ABTD_ythread_context *p_link =
+        ABTD_atomic_acquire_load_ythread_context_ptr(&p_ctx->p_link);
+    if (!p_link) {
+        uint32_t req = ABTD_atomic_fetch_or_uint32(&p_ythread->thread.request,
+                                                   ABTI_THREAD_REQ_JOIN);
+        if (!(req & ABTI_THREAD_REQ_JOIN)) {
+            /* This case means there is no join request. */
+            return NULL;
+        } else {
+            /* This case means a join request is issued and the joiner is
+             * setting p_link.  Wait for it. */
+            do {
+                p_link = ABTD_atomic_acquire_load_ythread_context_ptr(
+                    &p_ctx->p_link);
+            } while (!p_link);
+            return ABTI_ythread_context_get_ythread(p_link);
+        }
+    } else {
+        /* There is a join request. */
+        return ABTI_ythread_context_get_ythread(p_link);
+    }
+}
+
+static inline void ABTI_ythread_resume_joiner(ABTI_xstream *p_local_xstream,
+                                              ABTI_ythread *p_ythread)
+{
+    ABTI_ythread *p_joiner = ABTI_ythread_atomic_get_joiner(p_ythread);
+    if (p_joiner) {
+#ifndef ABT_CONFIG_ACTIVE_WAIT_POLICY
+        if (p_joiner->thread.type == ABTI_THREAD_TYPE_EXT) {
+            /* p_joiner is a non-yieldable thread (i.e., external thread). Wake
+             * up the waiter via the futex.  Note that p_arg is used to store
+             * futex (see thread_join_futexwait()). */
+            ABTD_futex_single *p_futex =
+                (ABTD_futex_single *)p_joiner->thread.p_arg;
+            ABTD_futex_resume(p_futex);
+            return;
+        }
+#endif
+        /* p_joiner is a yieldable thread */
+        ABTI_ythread_resume_and_push(ABTI_xstream_get_local(p_local_xstream),
+                                     p_joiner);
+    }
+}
+
 ABTU_noreturn static inline void
 ABTI_ythread_terminate(ABTI_xstream *p_local_xstream, ABTI_ythread *p_self)
 {
@@ -302,56 +351,39 @@ ABTI_ythread_terminate_to(ABTI_xstream *p_local_xstream, ABTI_ythread *p_self,
 ABTU_noreturn static inline void
 ABTI_ythread_exit(ABTI_xstream *p_local_xstream, ABTI_ythread *p_self)
 {
-    ABTD_ythread_context *p_ctx = &p_self->ctx;
-    ABTD_ythread_context *p_link =
-        ABTD_atomic_acquire_load_ythread_context_ptr(&p_ctx->p_link);
-    if (!p_link) {
-        uint32_t req = ABTD_atomic_fetch_or_uint32(&p_self->thread.request,
-                                                   ABTI_THREAD_REQ_JOIN);
-        if (!(req & ABTI_THREAD_REQ_JOIN)) {
-            /* This case means there is no join request.  Let's go back to the
-             * parent ULT */
-            ABTI_ythread_terminate(p_local_xstream, p_self);
+    ABTI_ythread *p_joiner = ABTI_ythread_atomic_get_joiner(p_self);
+    if (p_joiner) {
+#ifndef ABT_CONFIG_ACTIVE_WAIT_POLICY
+        if (p_joiner->thread.type == ABTI_THREAD_TYPE_EXT) {
+            /* p_joiner is a non-yieldable thread (i.e., external thread). Wake
+             * up the waiter via the futex.  Note that p_arg is used to store
+             * futex (see thread_join_futexwait()). */
+            ABTD_futex_single *p_futex =
+                (ABTD_futex_single *)p_joiner->thread.p_arg;
+            ABTD_futex_resume(p_futex);
+        } else
+#endif
+            if (p_self->thread.p_last_xstream ==
+                    p_joiner->thread.p_last_xstream &&
+                !(p_self->thread.type & ABTI_THREAD_TYPE_MAIN_SCHED)) {
+            /* Only when the current ULT is on the same ES as p_joiner's, we can
+             * jump to the joiner ULT.  Note that a parent ULT cannot be a
+             * joiner. */
+            ABTI_pool_dec_num_blocked(p_joiner->thread.p_pool);
+            ABTI_event_ythread_resume(ABTI_xstream_get_local(p_local_xstream),
+                                      p_joiner, &p_self->thread);
+            ABTI_ythread_terminate_to(p_local_xstream, p_self, p_joiner);
             ABTU_unreachable();
         } else {
-            /* This case means there has been a join request and the joiner has
-             * blocked.  We have to wake up the joiner ULT. */
-            do {
-                p_link = ABTD_atomic_acquire_load_ythread_context_ptr(
-                    &p_ctx->p_link);
-            } while (!p_link);
+            /* If the current ULT's associated ES is different from p_joiner's,
+             * we can't directly jump to p_joiner.  Instead, we wake up p_joiner
+             * here so that p_joiner's scheduler can resume it.  Note that the
+             * main scheduler needs to jump back to the root scheduler, so the
+             * main scheduler needs to take this path. */
+            ABTI_ythread_resume_and_push(ABTI_xstream_get_local(
+                                             p_local_xstream),
+                                         p_joiner);
         }
-    }
-    /* Now p_link != NULL. */
-    ABTI_ythread *p_joiner = ABTI_ythread_context_get_ythread(p_link);
-#ifndef ABT_CONFIG_ACTIVE_WAIT_POLICY
-    if (p_joiner->thread.type == ABTI_THREAD_TYPE_EXT) {
-        /* p_joiner is a non-yieldable thread (i.e., external thread). Wake up
-         * the waiter via the futex.  Note that p_arg is used to store futex
-         * (see thread_join_futexwait()). */
-        ABTD_futex_single *p_futex =
-            (ABTD_futex_single *)p_joiner->thread.p_arg;
-        ABTD_futex_resume(p_futex);
-    } else
-#endif
-        if (p_self->thread.p_last_xstream == p_joiner->thread.p_last_xstream &&
-            !(p_self->thread.type & ABTI_THREAD_TYPE_MAIN_SCHED)) {
-        /* Only when the current ULT is on the same ES as p_joiner's, we can
-         * jump to the joiner ULT.  Note that a parent ULT cannot be a joiner.
-         */
-        ABTI_pool_dec_num_blocked(p_joiner->thread.p_pool);
-        ABTI_event_ythread_resume(ABTI_xstream_get_local(p_local_xstream),
-                                  p_joiner, &p_self->thread);
-        ABTI_ythread_terminate_to(p_local_xstream, p_self, p_joiner);
-        ABTU_unreachable();
-    } else {
-        /* If the current ULT's associated ES is different from p_joiner's, we
-         * can't directly jump to p_joiner.  Instead, we wake up p_joiner here
-         * so that p_joiner's scheduler can resume it.  Note that the main
-         * scheduler needs to jump back to the root scheduler, so the main
-         * scheduler needs to take this path. */
-        ABTI_ythread_resume_and_push(ABTI_xstream_get_local(p_local_xstream),
-                                     p_joiner);
     }
     /* The waiter has been resumed.  Let's switch to the parent. */
     ABTI_ythread_terminate(p_local_xstream, p_self);
@@ -367,30 +399,7 @@ ABTI_ythread_exit_to(ABTI_xstream *p_local_xstream, ABTI_ythread *p_self,
      * execution and calls ythread_terminate/exit, this caller of this function
      * wants to jump to p_target.  Therefore, we should not context switch to
      * the joiner ULT. */
-    ABTD_ythread_context *p_ctx = &p_self->ctx;
-
-    if (ABTD_atomic_acquire_load_ythread_context_ptr(&p_ctx->p_link)) {
-        /* If p_link is set, it means that other ULT has called the join. */
-        ABTI_ythread *p_joiner = ABTI_ythread_context_get_ythread(
-            ABTD_atomic_relaxed_load_ythread_context_ptr(&p_ctx->p_link));
-        ABTI_ythread_resume_and_push(ABTI_xstream_get_local(p_local_xstream),
-                                     p_joiner);
-    } else {
-        uint32_t req = ABTD_atomic_fetch_or_uint32(&p_self->thread.request,
-                                                   ABTI_THREAD_REQ_JOIN);
-        if (req & ABTI_THREAD_REQ_JOIN) {
-            /* This case means there has been a join request and the joiner has
-             * blocked.  We have to wake up the joiner ULT. */
-            while (ABTD_atomic_acquire_load_ythread_context_ptr(
-                       &p_ctx->p_link) == NULL)
-                ;
-            ABTI_ythread *p_joiner = ABTI_ythread_context_get_ythread(
-                ABTD_atomic_relaxed_load_ythread_context_ptr(&p_ctx->p_link));
-            ABTI_ythread_resume_and_push(ABTI_xstream_get_local(
-                                             p_local_xstream),
-                                         p_joiner);
-        }
-    }
+    ABTI_ythread_resume_joiner(p_local_xstream, p_self);
     ABTI_ythread_terminate_to(p_local_xstream, p_self, p_target);
     ABTU_unreachable();
 }
@@ -403,30 +412,7 @@ static inline void ABTI_ythread_cancel(ABTI_xstream *p_local_xstream,
      * ULT has finished its execution and calls ythread_terminate/exit,
      * this function is called by the scheduler.  Therefore, we should not
      * context switch to the joiner ULT and need to always wake it up. */
-    ABTD_ythread_context *p_ctx = &p_ythread->ctx;
-
-    if (ABTD_atomic_acquire_load_ythread_context_ptr(&p_ctx->p_link)) {
-        /* If p_link is set, it means that other ULT has called the join. */
-        ABTI_ythread *p_joiner = ABTI_ythread_context_get_ythread(
-            ABTD_atomic_relaxed_load_ythread_context_ptr(&p_ctx->p_link));
-        ABTI_ythread_resume_and_push(ABTI_xstream_get_local(p_local_xstream),
-                                     p_joiner);
-    } else {
-        uint32_t req = ABTD_atomic_fetch_or_uint32(&p_ythread->thread.request,
-                                                   ABTI_THREAD_REQ_JOIN);
-        if (req & ABTI_THREAD_REQ_JOIN) {
-            /* This case means there has been a join request and the joiner has
-             * blocked.  We have to wake up the joiner ULT. */
-            while (ABTD_atomic_acquire_load_ythread_context_ptr(
-                       &p_ctx->p_link) == NULL)
-                ;
-            ABTI_ythread *p_joiner = ABTI_ythread_context_get_ythread(
-                ABTD_atomic_relaxed_load_ythread_context_ptr(&p_ctx->p_link));
-            ABTI_ythread_resume_and_push(ABTI_xstream_get_local(
-                                             p_local_xstream),
-                                         p_joiner);
-        }
-    }
+    ABTI_ythread_resume_joiner(p_local_xstream, p_ythread);
     ABTI_event_thread_cancel(p_local_xstream, &p_ythread->thread);
 }
 
